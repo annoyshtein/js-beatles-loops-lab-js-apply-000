261b1ecad574a8ff6ff8d625acbfb46041ee9b2a
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 17:32:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 		function theBeatlesPlay (musicians,instruments) {
 		  var beatles = []; 												// new empty array
-		    for (i = 0; i < 4; i++) { 							// for loop
+      var bLen = beatles.length;
+		    for (i = 0; i < bLen; i++) { 							// for loop
 				beatles.push(musicians[i] + " plays the " + instruments[i]); // add string elements to array of beatles and instruments
        	 	}
 			return beatles; 		// return array of strings
