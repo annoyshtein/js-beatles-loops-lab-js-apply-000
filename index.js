95153ada2f235442fcf20f9054cda2796fe7cf54
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 17:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+		var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay (musicians,instruments) {
+		function theBeatlesPlay (musicians,instruments) {
 
-  var beatles; // new empty array
-    for (i = 0; i < 3; i++) // for loop
-      beatles.push(musicians[i] + " plays the " + instruments[i]);
-      
-}
+		  var beatles = []; 												// new empty array
+		    for (i = 0; i < 4; i++) { 										// for loop
+				beatles.push(musicians[i] + " plays the " + instruments[i]); // add string elements to array of beatles and instruments
+       	 	}
+			return beatles; 		// return array of strings
+		}
