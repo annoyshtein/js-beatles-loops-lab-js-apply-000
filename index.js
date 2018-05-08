70cec7de01c4647271542baf87a54364cfd394b8
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 16:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay (musicians,instruments) 
