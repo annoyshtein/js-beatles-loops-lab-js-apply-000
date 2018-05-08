051143239c173ae897d2cdae67ba1df0f5a97b5b
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 17:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function theBeatlesPlay (musicians,instruments) {
-		  var beatles = []; 												// new empty array
-      var i;                                    // for loop counter
-		    for (i = 0; i < 4; i++) { 							// for loop
-				beatles.push(musicians[i] + " plays " + instruments[i]); // add string elements to array of beatles and instruments
-       	 	}
-			return beatles; 		// return array of strings
-		}
+  var beatles = []; 												// new empty array
+  var i;                                    // for loop counter
+	for (i = 0; i < 4; i++) { 							  // for loop
+	   beatles.push(musicians[i] + " plays " + instruments[i]); // add string elements to array of beatles and instruments
+      }
+  return beatles; 		// return array of strings
+	}
 
 function johnLennonFacts (facts) {
-  
+  var loudFacts = []; // new empty array
+  var i = 0; // loop counter
+  while (i < 4) {
+
+    i++;
+  }
 }
